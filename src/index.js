c5f0e885ba3a0ae1507e21d05c84a5eb796fbb2e
GusOdd <gus@dynama.by>
2021-02-22 21:18:25 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+  if (array.length === 0) {
+    return 0;
+  }
+  return Math.min.apply(null, array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+  if (array.length === 0) {
+    return 0;
+  }
+  return Math.max.apply(null, array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+  if (array.length === 0) {
+    return 0;
+  }
+  function reducer(acc, value) {
+    return acc + value;
+  }
+  return array.reduce(reducer) / array.length;
 }
